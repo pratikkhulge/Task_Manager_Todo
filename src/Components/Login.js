ade3408bc5fd4e09f91bd3063c4ab3ebda4695ae
Pratik khulge <113931760+pratikkhulge@users.noreply.github.com>
2024-02-07 23:13:54 +0530

    Update Login.js

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,59 +1,56 @@
-import React, { Component } from "react";
-// import logo from './logo.svg';
+import React from "react";
 import "../App.css";
 
-class Login extends Component {
-  render() {
-    return (
-      <div className="loginMainDiv" ref={this.props.loginCard}>
-        <div className="card z-depth-5">
-          <div className="card-content blue darken-2 white-text">
-            <p className="flow-text center-align">Log In</p>
-          </div>
-          <div className="card-content">
-            <div id="login">
-              <div className="row">
-                <form onSubmit={this.props.loginHandler} className="col s12">
-                  <div className="row">
-                    <div className="input-field col s12">
-                      <i className="material-icons prefix">email</i>
-                      <input
-                        ref={this.props.emailVal}
-                        id="loginEmail"
-                        type="email"
-                      />
-                      <label htmlFor="loginEmail">Email</label>
-                    </div>
-                  </div>
-                  <div className="row">
-                    <div className="input-field col s12">
-                      <i className="material-icons prefix">lock</i>
-                      <input
-                        ref={this.props.passVal}
-                        id="password"
-                        type="password"
-                      />
-                      <label htmlFor="password">Password</label>
-                    </div>
-                  </div>
-                  <button
-                    type="submit"
-                    className="btn waves-effect waves-light pink right"
-                  >
-                    Log In
-                    <i className="material-icons right">send</i>
-                  </button>
-                </form>
-              </div>
-            </div>
+const Login = ({ loginHandler, switchToSignUp }) => {
+  return (
+    <div className="loginMainDiv">
+      <div className="card z-depth-5">
+        <div className="card-content blue darken-2 white-text">
+          <p className="flow-text center-align">Log In</p>
+        </div>
+        <div className="card-content">
+          <div id="login">
             <div className="row">
-                            <p className="center-align">Dont have an account?   <button onClick={this.props.switchToSignUp}className="transparent-btn blue-text">SignUp</button></p>
-                        </div>
+              <form onSubmit={loginHandler} className="col s12">
+                <div className="row">
+                  <div className="input-field col s12">
+                    <i className="material-icons prefix">email</i>
+                    <input id="loginEmail" type="email" name="emailVal" />
+                    <label htmlFor="loginEmail">Email</label>
+                  </div>
+                </div>
+                <div className="row">
+                  <div className="input-field col s12">
+                    <i className="material-icons prefix">lock</i>
+                    <input id="password" type="password" name="passVal" />
+                    <label htmlFor="password">Password</label>
+                  </div>
+                </div>
+                <button
+                  type="submit"
+                  className="btn waves-effect waves-light pink right"
+                >
+                  Log In
+                  <i className="material-icons right">send</i>
+                </button>
+              </form>
+            </div>
+          </div>
+          <div className="row">
+            <p className="center-align">
+              Dont have an account?{" "}
+              <button
+                onClick={switchToSignUp}
+                className="transparent-btn blue-text"
+              >
+                SignUp
+              </button>
+            </p>
           </div>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default Login;
