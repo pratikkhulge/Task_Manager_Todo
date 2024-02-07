061ee6e926d8b02b8c8267e440b98919e3473c5d
Pratik khulge <113931760+pratikkhulge@users.noreply.github.com>
2024-02-07 23:18:35 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -7,15 +7,16 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyBvf17nDo6sNZmf9tEBHI6ABsboiR8pQQk",
-    authDomain: "firstapp-81884.firebaseapp.com",
-    databaseURL: "https://firstapp-81884-default-rtdb.firebaseio.com",
-    projectId: "firstapp-81884",
-    storageBucket: "firstapp-81884.appspot.com",
-    messagingSenderId: "640215432215",
-    appId: "1:640215432215:web:52a2c185664dd83d90b052",
-    measurementId: "G-JWCKZFRN31"
+    apiKey: "your-api-key",
+    authDomain: "your-auth-domain",
+    databaseURL: "your-database-url",
+    projectId: "your-project-id",
+    storageBucket: "your-storage-bucket",
+    messagingSenderId: "your-sender-id",
+    appId: "your-sender-id",
+    measurementId: "your-app-id"
   };
+
 
   const app = initializeApp(firebaseConfig);
   export const auth = getAuth(app);
